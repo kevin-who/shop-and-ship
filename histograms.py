529c11d9e63057ce0c2ee3e5a36b15ce44ffd556
Kevin Hu <kevinhuwest@gmail.com>
2016-11-10 14:02:00 -0500

    pixel counts

--- histograms.py
+++ histograms.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import math
 import pandas as pd
 
-pixel_counts = pd.read_csv("pixel_counts.csv")
+pixel_counts = pd.read_csv("k_pixel_counts.csv")
 plt.figure(figsize=(60, 30))
 sns.distplot(pixel_counts["pixels"],bins=100, rug=False)
 plt.savefig("histogram.png")
--- pixel_counts.py
+++ pixel_counts.py
@@ -1,7 +1,8 @@
 import numpy as np
 import numpy.ma as ma
+import csv
 
-pixel_counts = {}
+pixel_counts = []
 
 for i in range(1,874):
 	print(i)
@@ -9,4 +10,10 @@ for i in range(1,874):
 	map_matrix = np.loadtxt(fn)
 	map_matrix = [list(x) for x in map_matrix]
 	count = sum(x.count(255) for x in map_matrix)
-	pixel_counts[str(i)] = count
+	pixel_counts.append(count)
+
+with open('k_pixel_counts.csv', 'w') as f:
+	w = csv.DictWriter(f,["region","pixels"])
+	w.writeheader()
+	for i in range(0,873):
+		w.writerow({"region":i+1,"pixels":pixel_counts[i]})
