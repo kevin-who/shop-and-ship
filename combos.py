800c561eb233f37084878a33ea9696986ff45dbb
Kevin Hu <kevinhuwest@gmail.com>
2016-11-10 17:56:25 -0500

    filter pngs

--- combos.py
+++ combos.py
@@ -1,9 +1,18 @@
 import numpy as np
 from bitstring import BitArray
 
-fn = 'binary_arrays/' + str(1) + '.txt'
+fn = 'binary_arrays/' + str(437) + '.txt'
 with open(fn, 'r') as bits:
 	bits_string = bits.read()
 
-bits_string = BitArray(bin=bits_string)
-print(bits_string)
+bits_string1 = BitArray(bin=bits_string)
+
+fn = 'binary_arrays/' + str(322) + '.txt'
+with open(fn, 'r') as bits:
+	bits_string = bits.read()
+
+bits_string2 = BitArray(bin=bits_string)
+
+combo = bits_string1 | bits_string2
+
+print(combo.count("1"))
---
+++ filter_bad_pngs.py
@@ -0,0 +1,13 @@
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import pandas as pd
+import os
+
+bad_regions = list(np.loadtxt("bad_regions.txt"))
+bad_regions = [int(x) for x in bad_regions]
+print(bad_regions)
+
+for i in bad_regions:
+	os.remove("pngs/" + str(i) + ".png")
