11b33ec379b0dab01ffd2bd37eef28d25c01ab6e
Kevin Hu <kevinhuwest@gmail.com>
2016-11-10 23:53:27 -0500

    map maker

--- map_maker.py
+++ map_maker.py
@@ -1,28 +1,38 @@
+import tensorflow as tf
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from bitstring import BitArray
 
+with tf.Graph().as_default():
+	with tf.Session() as sess:
+		map_string = "484_241_431_442_109_561_519_390_346_216_209_444_322_506_168"
+		region_ids = [int(x) for x in map_string.split("_")]
+		print(region_ids)
 
-map_string = "484_241_431_442_109_561_519_390_346_216_209_444_322_506_168"
-region_ids = [int(x) for x in map_string.split("_")]
-print(region_ids)
+		fn = 'binary_arrays/' + str(region_ids[0]) + '.txt'
+		with open(fn, 'r') as bits:
+			bits_string = bits.read()
+			map_bin = BitArray(bin=bits_string)
 
-fn = 'binary_arrays/' + str(region_ids[0]) + '.txt'
-with open(fn, 'r') as bits:
-	bits_string = bits.read()
-	map_bin = BitArray(bin=bits_string)
+		all_or = map_bin
 
-all_or = map_bin
+		for i in region_ids:
+			fn = 'binary_arrays/' + str(i) + '.txt'
 
-for i in region_ids:
-	fn = 'binary_arrays/' + str(i) + '.txt'
+			with open(fn, 'r') as bits:
+				bits_string = bits.read()
+				bit_array = BitArray(bin=bits_string)
 
-	with open(fn, 'r') as bits:
-		bits_string = bits.read()
-		bit_array = BitArray(bin=bits_string)
+			all_or = all_or | bit_array
 
-	all_or = all_or | bit_array
-
-bit_array = [int(x) for x in list(str(all_or.bin))]
-print(bit_array)
+		bit_array = [int(x) for x in list(str(all_or.bin))]
+		bit_array = np.array(bit_array)*255
+		tensor = bit_array.astype(np.uint8)
+		tensor = tf.reshape(tensor,[353,546,1])
+		output_image = tf.image.encode_png(tensor)
+		init_op = tf.initialize_all_variables()
+		#save the output
+		f = open("map_maker/" + map_string + ".png", "wb+")
+		f.write(output_image.eval())
+		f.close()
